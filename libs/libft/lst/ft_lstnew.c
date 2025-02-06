e67fd4650ca70b54bfb7c70fac4c893be7196a2f
Matheus Infingardi <matheus.infingardicr@gmail.com>
2025-02-06 14:39:23 -0300

    libft

---
+++ libs/libft/lst/ft_lstnew.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minfinga <minfinga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/04 20:43:37 by minfinga          #+#    #+#             */
+/*   Updated: 2024/05/20 23:23:16 by minfinga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new -> content = content;
+	new -> next = NULL;
+	return (new);
+}
